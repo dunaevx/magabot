0155fc83f1753bc71340ed6bb288c00801cb79b3
Хамид <hamiddunaev@gmail.com>
2025-12-10 22:26:48 +0600

    update model

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = "8256189886:AAEs4rtnkSizyLFMoQRNUrL84Ip1uOHgjTM"
-OPENROUTER_API_KEY = "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"
+OPENROUTER_API_KEY = "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"
 OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1/chat/completions"
-OPENROUTER_MODEL = "meituan/longcat-flash-chat:free"
+OPENROUTER_MODEL = "meta-llama/llama-3.3-70b-instruct:free"
 
 # longcat-flash-chat:free = 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
 # deepseek/deepseek-chat-v3.1:free = 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
